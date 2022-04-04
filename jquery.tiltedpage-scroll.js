42dd273495e08fbccf48c3748fbdd748f43ff4f3
Uttam <uttam.kumar.97091@gmail.com>
2022-04-04 20:39:08 +0530

    conneted to main website

--- jquery.tiltedpage-scroll.js
+++ jquery.tiltedpage-scroll.js
@@ -1,18 +1,3 @@
-/* ===========================================================
- * jquery-tiltedpage-scroll.js v1.2.1
- * ===========================================================
- * Copyright 2013 Pete Rojwongsuriya.
- * http://www.thepetedesign.com
- *
- * Create a beautiful 3D tilted effect on scroll
- * with Tilted Page Scroll plugin
- *
- * https://github.com/peachananr/tiltedpage-scroll
- * 
- * License: GPL v3
- *
- * ========================================================== */
-
 !function($){
   
   var defaults = {
